1f1d42b81956f7b115101b8381ea739ab1901e73
krzysztofserocki <leicer141@gmail.com>
2019-03-18 18:26:23 +0100

    Connect to controller and navigate to details

--- src/sample/controller/LoginController.java
+++ src/sample/controller/LoginController.java
@@ -6,7 +6,12 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -37,9 +42,33 @@ public class LoginController {
         loginButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Login was clicked!");
+
+                loginUser();
+
             }
         });
+    }
+
+    private void loginUser() {
+
+        // Get the main window
+        loginButton.getScene().getWindow().hide();
+
+        if(!loginUsername.getText().toString().trim().equals("")
+                && !loginPassword.getText().toString().trim().equals("")) {
+            // We are ready to go!
+            Stage detailsStage = new Stage();
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/sample/view/details.fxml"));
+
+                Scene scene = new Scene(root);
+                detailsStage.setScene(scene);
+                detailsStage.show();
+                detailsStage.setResizable(false);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
